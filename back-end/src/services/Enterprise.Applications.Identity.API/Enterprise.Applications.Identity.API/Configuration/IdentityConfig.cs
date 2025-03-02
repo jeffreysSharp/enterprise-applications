2762f45ea9333457ce8ba3797aff6befbe66383a
Jeferson Almeida <jefferson_qi3@hotmail.com>
2025-03-02 15:36:52 -0300

    Abstracting Configurations

---
+++ back-end/src/services/Enterprise.Applications.Identity.API/Enterprise.Applications.Identity.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,54 @@
+﻿using Enterprise.Applications.Application.Common.Interfaces;
+using Enterprise.Applications.Identity.Infra.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Enterprise.Applications.Identity.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            var _key = configuration["Jwt:Key"];
+            var _issuer = configuration["Jwt:Issuer"];
+            var _audience = configuration["Jwt:Audience"];
+            var _expirtyMinutes = configuration["Jwt:ExpiryMinutes"];
+                        
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key)),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = _audience,
+                    ValidIssuer = _issuer,
+                    ClockSkew = TimeSpan.FromMinutes(Convert.ToDouble(_expirtyMinutes))
+                };
+            });
+
+            services.AddSingleton<ITokenGenerator>(new TokenGenerator(_key, _issuer, _audience, _expirtyMinutes));
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            return app;
+        }
+    }
+}
---
+++ back-end/src/services/Enterprise.Applications.Identity.API/Enterprise.Applications.Identity.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Enterprise.Applications.Identity.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Enterprise Applications Identity API",
+                    Description = "This API is responsible for user management in the Enterprise Applications project.",
+                    Contact = new OpenApiContact() { Name = "Jeferson Almeida", Email = "jeffreys.sharp@outlook.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/license/mit") }
+                });
+
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+
+    }
+}
